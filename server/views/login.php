7efa623ea3f7028b89b94dd0509f59144a0222e0
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2012-10-18 10:16:51 +0200

    Added a bunch of icons!

--- server/views/login.php
+++ server/views/login.php
@@ -13,6 +13,8 @@
 	<meta name="description" content="kloenschnack — really simple team messaging">
 	<meta name="author" content="Marc-Oliver Teschke">
 	<meta name = "viewport" content = "initial-scale=1.0">
+	<link rel="apple-touch-icon" href="/images/apple-touch-icon-precomposed.png">
+	<link rel="shortcut icon" href="/images/favicon.ico" />
 	<link rel="stylesheet" href="/min/?b=stylesheets&f=normalize.css,font-awesome.css,kloenschnack.css">
 	<!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
