9ca404798ef6f7c4c9a413466a040bfbc4fff67f
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2013-08-23 15:48:04 +0200

    Added a bunch of users.

--- sql/0003.sql
+++ sql/0003.sql
@@ -11,3 +11,8 @@ ALTER TABLE users ADD COLUMN last_login INT NOT NULL DEFAULT 0;
 ALTER TABLE users ADD COLUMN last_activity INT NOT NULL DEFAULT 0;
 
 INSERT INTO users VALUES(DEFAULT, 'sebastian', 'Sebastian Meister', '$2rcByx51ejoM', 0, 0);
+
+
+INSERT INTO users VALUES(DEFAULT, 'jens', 'Jens Eversmann', '$2rcByx51ejoM', 0, 0);
+INSERT INTO users VALUES(DEFAULT, 'cathrin', 'Cathrin Lange', '$2rcByx51ejoM', 0, 0);
+INSERT INTO users VALUES(DEFAULT, 'martina', 'Martina Seiler', '$2rcByx51ejoM', 0, 0);
