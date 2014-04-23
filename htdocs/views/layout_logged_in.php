dd090fe7d9d7fa6359a33209856b2255cd1d7f66
Marc-Oliver Teschke <teschke@planwerk6.de>
2014-04-23 12:05:50 +0200

    HTML framework in separate layout file.

---
+++ htdocs/views/layout_logged_in.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php print $page_title ?></title>
+	<meta name="description" content="kloenschnack — really simple team messaging">
+	<meta name="author" content="Marc-Oliver Teschke">
+	<meta name = "viewport" content = "initial-scale=1.0">
+	<link rel="apple-touch-icon" href="/images/apple-touch-icon-precomposed.png">
+	<link rel="shortcut icon" href="/images/favicon.ico" />
+	<link rel="stylesheet" href="/min/?b=stylesheets&f=normalize.css,jquery.cssemoticons.css,font-awesome.css,kloenschnack.css">
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<?php print $body_content; ?>
+</body>
+</html>
