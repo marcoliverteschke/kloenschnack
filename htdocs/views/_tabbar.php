e9fa04d116ceca7dec67fe7bc2fa1488e440adf8
Marc-Oliver Teschke <teschke@planwerk6.de>
2014-04-25 11:17:03 +0200

    Moved JavaScript, tabs are in their own file now.

---
+++ htdocs/views/_tabbar.php
@@ -0,0 +1,9 @@
+<section class="tabbar hanging-tabs">
+	<section class="container">
+		<ul class="clearfix">
+			<li><a href="/archive">Suche</a></li>
+			<li><a href="/settings">Einstellungen</a></li>
+			<li><a href="/logout">abmelden</a></li>
+		</ul>
+	</section>
+</section>
--- htdocs/views/app.php
+++ htdocs/views/app.php
@@ -1,11 +1,4 @@
-<section class="tabbar hanging-tabs">
-	<section class="container">
-		<ul class="clearfix">
-			<li><a href="/settings">Einstellungen</a></li>
-			<li><a href="/logout">abmelden</a></li>
-		</ul>
-	</section>
-</section>
+<?php include('_tabbar.php'); ?>
 <section class="drawers">
 	<section class="drawer users">
 		<ul>
@@ -41,24 +34,3 @@
 		</form>
 	</section>
 </section>
-<script>var default_document_title = '<?php print $page_title ?>';</script>
-<script>var user_id = '<?php print $user_id ?>';</script>
-<script src="/javascript/jquery.js"></script>
-<script src="/min/?b=javascript&f=handlebars.js,underscore.js,modernizr.js,jquery.cookies.2.2.0.min.js,jquery.cssemoticons.min.js,md5.js"></script>
-<script src="/min/?b=javascript/phpjs&f=date.js,get_html_translation_table.js,html_entity_decode.js,htmlentities.js,nl2br.js,trim.js"></script>
-<script src="/min/?b=javascript/models&f=Post.js,PostsQueue.js"></script>
-<script src="/min/?b=javascript/fileupload&f=vendor/jquery.ui.widget.js,jquery.iframe-transport.js,jquery.fileupload.js"></script>
-
-<script src="/min/?b=javascript&f=kloenschnack.js"></script>
-<script id="post-template" type="text/x-handlebars-template">
-	<article class="timeline-entry" data-created="{{created}}">
-		<span class="person">{{#if author}}{{author}} sagte {{humanTime created}}{{else}}{{humanTimeShorter created}}{{/if}}: </span>
-		{{#if multiline}}<pre class="post {{#if at_me}}at_me{{/if}}">{{else}}<p class="post {{#if at_me}}at_me{{/if}}">{{/if}}{{{body}}}{{#if multiline}}</pre>{{else}}</p>{{/if}}
-	</article>
-</script>
-<script id="event-template" type="text/x-handlebars-template">
-	<div class="event">{{author}} {{{body}}} {{humanTime created}}</div>
-</script>
-<script id="list-entry-template" type="text/x-handlebars-template">
-	<li class="{{class}}" title="{{title}}">{{{name}}}</li>
-</script>
